4839a272752aa73045b1b359d6b21283ec220d30
DianaCato <diana.catolico@hotmail.com>
2021-08-04 16:25:56 -0500

    valid positions

--- src/API/api.js
+++ src/API/api.js
@@ -1,7 +1,32 @@
+let errorLetter;
+
+const letterMesagge = (letter) => {
+  let mesagge;
+  switch (letter) {
+    case 'V':
+      mesagge = 'V (5)';
+      break;
+    case 'L':
+      mesagge = 'L (50)';
+      break;
+    case 'D':
+      mesagge = 'D (500)';
+      break;
+    default:
+      mesagge = 'Not error';
+      break;
+  }
+  return mesagge;
+};
+
 const isValidLetters = (stringRomans) => {
   const romans = ['V', 'L', 'D'];
   const count = romans.map((letter) => stringRomans.filter((e) => e === letter).length);
   const valid = count.every((n) => n < 2);
+  count.filter((e, index) => {
+    if (e >= 2) errorLetter = romans[index];
+    return null;
+  });
   return valid;
 };
 
@@ -9,6 +34,10 @@ const isValidRepetition = (stringRomans) => {
   const romans = ['I', 'X', 'C', 'M'];
   const count = romans.map((letter) => stringRomans.filter((e) => e === letter).length);
   const isValid = count.every((n) => n < 4);
+  count.filter((e, index) => {
+    if (e >= 4) errorLetter = romans[index];
+    return null;
+  });
   return isValid;
 };
 
@@ -22,6 +51,10 @@ const isValidSubstraction = (roman) => {
   const romans = ['VX', 'VL', 'VC', 'VD', 'VM', 'LC', 'LD', 'LM', 'DM'];
   const exist = romans.map((e) => roman.indexOf(e));
   const valid = exist.every((n) => n < 0);
+  exist.filter((e, index) => {
+    if (e >= 0) errorLetter = romans[index];
+    return null;
+  });
   return valid;
 };
 
@@ -43,14 +76,14 @@ module.exports.parse = (roman) => {
     throw new Error('Unknown roman numeral');
   }
   if (!isValidLetters(arrayRomans)) {
-    throw new Error('Invalid repetition of number starting with 5: V (5), L (50), D (500)');
+    throw new Error(`Invalid repetition of number starting with 5: ${letterMesagge(errorLetter)}`);
   }
 
   if (!isValidRepetition(arrayRomans)) {
-    throw new Error('Too many repetitions of roman numeral I, X, C, M');
+    throw new Error(`Too many repetitions of roman numeral ${errorLetter}`);
   }
   if (!isValidSubstraction(romanCapital)) {
-    throw new Error('Invalid substraction prefix V, L, D');
+    throw new Error(`Invalid substraction prefix ${errorLetter[0]}`);
   }
   if (!isValidPosition(romanCapital)) {
     throw new Error('Invalid order');
