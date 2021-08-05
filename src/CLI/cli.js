0867ee9e2c482b637a61eefcd32947e410b29a68
DianaCato <diana.catolico@hotmail.com>
2021-08-05 18:13:07 -0500

    cli

--- src/CLI/cli.js
+++ src/CLI/cli.js
@@ -2,13 +2,20 @@
 const program = require('commander');
 const { parse: convertParse, stringify } = require('../API/api');
 
+const cli = (name) => {
+  const arg = process.argv[3];
+  if (name === 'stringify') {
+    console.log(stringify(+arg));
+  }
+  if (name === 'parse') {
+    console.log(convertParse(arg));
+  }
+};
+
 program
   .version('0.1.0')
-  .arguments('<number>')
+  .arguments('<number>', '<string>')
   .option('--parse')
-  .option('--stringify');
+  .option('--stringify')
+  .action(cli);
 program.parse(process.argv);
-
-const options = program.opts();
-
-console.log('gaaaaaa', options);
