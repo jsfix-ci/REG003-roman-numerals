a3522a7086fcc0c3a0eb287deb4341ad2beb77ad
Briceyda <Brd0208@gmail.com>
2021-08-06 13:14:51 -0500

    coomplete functions CLI

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
+//
 const { parse } = require("../src/parse");
-const { stringify } = require("../src/stringify");
+const { stringify } = require("../src/stringify")
 // Librería nos ayuda a imprimir en color en la terminal
 const chalk = require("chalk");
 // Librería para crear banners con caracteres y símbolos
@@ -11,86 +12,47 @@ program.version('0.0.1');
 // process.argv contiene todos los argumentos de la linea de comando
 const args = process.argv;
 
-// 1.- Tendremos un comando llamado roman-numerals
-// 2.- El comando tomará cuatro argumentos: 
-// help, parse, stringify, --version
+// Mostrar un banner con un mensaje formado por caracteres.
+const msn = msn => {
+  console.log(chalk.bold.cyan(figlet.textSync(msn, {
+    font: 'ANSI Shadow',
+    horizontalLayout: 'default',
+    verticalLayout: 'default'
+  })));
+}
+// IIFE (Immediately Invoked Function Expression)
+(async () => {
+  msn('Roman Numerals - CLI');
+})();
+
 program
-  .option("-d, --debug", "output extra debugging")
-  .version("0.1.0")
-  .argument("<username>", "user to login")
-  .action((username, password) => {
-    console.log("username:", username);
-    console.log("password:", password);
+  .command("parse")
+  .argument("<string>")
+  .description("Converts a roman number to arabic, only receives elements of type string.")
+  .action((name) => {
+    try{
+      console.log(parse(name))
+    } catch(error) {
+      console.log(error.message)
+    }
+    
   });
-program.parse(process.argv);
 
-const options = program.opts();
-if (options.debug) console.log(options);
+program
+  .command("stringify")
+  .argument("<number>")
+  .description(
+    "Converts a arabic number to roman, only receives elements of type number"
+  )
+  .action((name) => {
+    try {
+      console.log(stringify(+name));
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
+program.parse(args);
 
 
 
 
-
-
-
-
-
-
-
-
-
-// // // Librería que ayuda al usuario a elegir opciones
-// const inquirer = require("inquirer");
-
-// console.log(args[2]);
-// if (args.length <= 3 && (args[2] == ("parse" || "stringify"))) {
-//   inquirer
-//     .prompt({
-//       type: "input",
-//       name: "value",
-//       message: "Please enter a roman number",
-//     })
-//     .then((answer) => {
-//       console.log(parse(answer.value));
-//     });
-//     if(args[2] ==  ) {
-//       inquirer
-//     .prompt({
-//       type: "input",
-//       name: "value",
-//       message: "Please enter a roman number",
-//     })
-//     .then((answer) => {
-//       console.log(stringify(answer.value));
-//     });
-//     }
-// } else {
-//   console.log("oh no");
-// }
-
-// //console.log(args)
-
-// // // Mostrar un banner con un mensaje formado por caracteres.
-// // const msn = msn => {
-// //   console.log(chalk.bold.cyan(figlet.textSync(msn, {
-// //     font: 'ANSI Shadow',
-// //     horizontalLayout: 'default',
-// //     verticalLayout: 'default'
-// //   })));
-// // }
-// // // IIFE (Immediately Invoked Function Expression)
-// // (async () => {
-// //   msn('Roman Numerals - CLI');
-// // })();
-
-// //
-// // inquirer
-// //   .prompt({
-// //     type: "list",
-// //     name: "colors",
-// //     message: "Which are your favorite color?",
-// //     choices: ["red", "blue", "green"],
-// //   })
-// //   .then((answer) => {
-// //     console.log(answer);
-// //   });
---
+++ bin/parse.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+const { parse } = require("../src/parse");
+
+// process.argv contiene todos los argumentos de la linea de comando
+const args = process.argv;
+console.log(args)
